65ef4b07c0839142da8a3bde3cc52525918878a4
Oséias Medeiros <oseiasmed@yahoo.com.br>
2020-05-21 22:20:35 -0300

    Finalização de CRUD de pacientes - CRUD completo

--- controllers/pacientesController.js
+++ controllers/pacientesController.js
@@ -1,11 +1,31 @@
 const express = require("express");
 
-const authentication = require("../middlewares/adminauth");
 const Paciente = require("../models/Paciente");
+
+// Criar pacientes
+
+exports.store = (req, res, next) => {
+
+    var { nome, cpf, endereco, numero, dtnascimento, complemento, bairro, cidade, uf } = req.body;
+
+    Paciente.create({
+
+        nome, cpf, endereco, numero, dtnascimento, complemento, bairro, cidade, uf
+
+    }).then(() => {
+
+        res.status(200).send('Paciente criado com sucesso');
+
+    }).catch(error => {
+
+        res.status(400).send('Erro ao criar o paciente');
+
+    });
+};
 
 // Listar pacientes
 
-exports.index = (req, res) => {
+exports.index = (req, res, next) => {
 
     Paciente.findAll().then(pacientes => {
 
@@ -16,7 +36,69 @@ exports.index = (req, res) => {
         res.status(400).send('Erro ao listar os pacientes');
 
     });
-}
+};
+
+// Editar pacientes
+
+exports.update = (req, res) => {
+
+    var id = req.params.id;
+    var { nome, cpf, endereco, numero, dtnascimento, complemento, bairro, cidade, uf } = req.body;
+
+    Paciente.update({
+
+        nome, cpf, endereco, numero, dtnascimento, complemento, bairro, cidade, uf
+
+    }, {
+
+        where: {
+
+            id: id
+        }
+
+    }).then(() => {
+
+        res.status("200").send("Paciente editado com sucesso");
+
+    }).catch(error => {
+
+        res.status(400).send("Erro ao editar paciente " + error)
+
+    })
+};
+
+// Deletar pacientes
+
+exports.delete = (req, res, next) => {
+
+    var id = req.body.id;
+
+    if (id != undefined) {
+
+        if (!isNaN(id)) {
+
+            Paciente.destroy({
+
+                where: {
+                    id: id
+                }
+            }).then(() => {
+
+                res.status(200);
+                res.send("Paciente removido com sucesso.")
+            });
+
+        } else {
+
+            res.status(400);
+        }
+
+    } else {
+
+        res.status(400);
+    }
+};
+
 
 
 
--- routes/pacientesRoutes.js
+++ routes/pacientesRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const PacientesController = require("../controllers/PacientesController");
 
-router.get('/pacientes',PacientesController.index);
+router.get('/pacientes', PacientesController.index);
+router.post('/pacientes', PacientesController.store);
+router.put('/pacientes/:id', PacientesController.update);
+router.delete('/pacientes', PacientesController.delete);
 
-
-module.exports = router; 
+module.exports = router;
 
